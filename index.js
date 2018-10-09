774444db05c0430cb3e4925ffc3ac5379d8c182c
wlsdud2194 <wlsdud2194@naver.com>
2018-10-09 20:38:45 +0900

    Requore
    COomit

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-const content = document.getElementById("js_Content");
 
-function Upload (e){
+//목록 ajax 처리
+function Upload (e) {
     let list = ["./videoTemplate", "./writeTemplate"];
-
-    fetch(list[e]).then(function(response){
-        response.text().then(function(text){
-            content.innerHTML=text;
-        })
-    })
+    $.ajax({
+        url: list[e], 
+        success: function(result){
+            $("#js_Content").html(result);
+        },
+        error: function(xhr, status, error){
+            console.log(status + ":  " + error);
+        }
+        
+    });
 }
 
+// 공지창 닫기
 function Popup_Close(){
     let CButton = document.getElementById("js_NoticeWrap");
     CButton.style.display = 'none';
 }
+
+
+
+// const content = document.getElementById("js_Content");
+
+// function Upload (e){
+//     let list = ["./videoTemplate", "./writeTemplate"];
+
+//     fetch(list[e]).then(function(response){
+//         response.text().then(function(text){
+//             content.innerHTML=text;
+//         })
+//     })
+// }
