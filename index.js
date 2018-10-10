464357c88c5c2daf3348869544d8bb07e60f77ec
wlsdud2194 <wlsdud2194@naver.com>
2018-10-10 22:36:53 +0900

    LastCommit(1)
    form 수정은 알아서

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 //목록 ajax 처리
 function Upload (e) {
-    let list = ["./videoTemplate", "./writeTemplate"];
+    let list = ["./videoTemplate.html", "./writeTemplate.html"];
     $.ajax({
         url: list[e], 
         success: function(result){
@@ -15,27 +15,39 @@ function Upload (e) {
 }
 
 // 공지창 닫기
-// function Popup_Close(){
-//     let CButton = document.getElementById("js_NoticeWrap");
-//     CButton.style.display = 'none';
-// }
-
-// $("#js_SubmitButton").on('click', function(){
-//     let message = document.querySelector(".Post_Sec textarea").value;
-//     if("" == message){
-//         $("#AlertPage").show();
-//     }
-// })
+function Popup_Close(){
+    let CButton = document.getElementById("js_NoticeWrap");
+    CButton.style.display = 'none';
+}
+//알림창 닫기
+function Alert_Close(){
+    $(".AlertLayer").css("display", "none").css("visibility", "hide");
+    return 1;
+}
 
 
-// const content = document.getElementById("js_Content");
+//Post submit
+function js_SubmitBtn_onclick(){
+    var message = $(".js_Paper").val().length;
+    if(message == 0){
+        $(".AlertLayer").css("display", "block").css("visibility", "visible");
+    }
+    else{
+        alert("올리기 성공");
+        $(".js_SubmitBtn").attr('type', "submit")
+    }
+    return 1;
+}
 
-// function Upload (e){
-//     let list = ["./videoTemplate", "./writeTemplate"];
 
-//     fetch(list[e]).then(function(response){
-//         response.text().then(function(text){
-//             content.innerHTML=text;
-//         })
-//     })
-// }
+    // const content = document.getElementById("js_Content");
+
+    // function Upload (e){
+    //     let list = ["./videoTemplate", "./writeTemplate"];
+
+    //     fetch(list[e]).then(function(response){
+    //         response.text().then(function(text){
+    //             content.innerHTML=text;
+    //         })
+    //     })
+    // }
